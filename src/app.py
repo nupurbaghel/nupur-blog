332779d35e63e52538151d46d064763e58fb6693
Nupur Baghel <nupurbaghel@gmail.com>
2016-06-21 01:57:13 +0530

    First completed version

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ app.secret_key="nupur"
 
 @app.route('/')
 def home_template():
-    return render_template('home.html',email=session['email'])
+    return render_template('home.html')
 
 @app.route('/login')
 def login_template():
